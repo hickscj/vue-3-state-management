18a325486372b4dc303dda0351c88bacc8a0ad1f
Chad Hicks <chad@meetmaestro.com>
2021-10-16 10:44:09 -0400

    Add initial vuex store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,8 @@
 import Product from "../components/Product.vue";
 import Cart from "../components/Cart.vue";
 
-import { ref } from "vue";
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
   name: 'Home',
@@ -31,56 +32,19 @@ export default {
     Cart
   },
   setup(){
+      const store = useStore();
 
-    let products = ref();
+      let products = computed(function() {
+          return store.state.products
+      });
 
-    products = [
-        {
-          id : 1,
-          name : "Chelsea Shoes",
-          price : 200,
-          shortdesc : "Best Drip in the Market",
-          url : "images/chelsea-shoes.png"
-        },
-        {
-          id : 2,
-          name : "Kimono",
-          price : 50,
-          shortdesc : "Classy, Stylish, Dope",
-          url : "images/kimono.png"
-        },
-        {
-          id : 3,
-          name : "Watch",
-          price : 2500,
-          shortdesc : "Elegance built in",
-          url : "images/rolex.png"
-        },
-        {
-          id : 4,
-          name : "Wallet",
-          price : 80,
-          shortdesc : "Sleek, Trendy, Clean",
-          url : "images/wallet.png"
-        },
-        {
-          id : 5,
-          name : "Lady Handbags",
-          price : 230,
-          shortdesc : "Fabulous, Exotic, Classy",
-          url : "images/handbag.png"
-        },
-        {
-          id : 6,
-          name : "Casual Shirts",
-          price : 30,
-          shortdesc : "Neat, Sleek, Smart",
-          url : "images/shirt.png"
-        }
-    ]
+      let cart = computed(function() {
+          return store.state.cart
+      });
 
     return {
-      products
+      products,
+      cart
     }
   },
   
